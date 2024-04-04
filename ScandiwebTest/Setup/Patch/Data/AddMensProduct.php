6daf841531c3bfc1fa756c08e361b483ac7fd558
Rihards Pokulis <rihards.pokulis@scandiweb.com>
2024-04-04 11:51:18 +0300

    second commit

--- ScandiwebTest/Setup/Patch/Data/AddMensProduct.php
+++ ScandiwebTest/Setup/Patch/Data/AddMensProduct.php
@@ -1,81 +1,138 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Scandiweb\ScandiwebTest\Setup\Patch\Data;
 
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\CategoryLinkManagementInterface;
-use Magento\Catalog\Api\CategoryRepositoryInterface;
 use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
 
 class AddMensProduct implements DataPatchInterface
 {
-    private $moduleDataSetup;
-    private $productRepository;
-    private $productFactory;
-    private $categoryLinkManagement;
-    private $categoryRepository;
-    private $state;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $productRepository;
 
+    /**
+     * @var ProductInterfaceFactory
+     */
+    protected $productFactory;
+
+    /**
+     * @var CategoryLinkManagementInterface
+     */
+    protected $categoryLinkManagement;
+
+    /**
+     * @var State
+     */
+    protected $state;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected $sourceItemsSaveInterface;
+
+    /**
+     * AddMensProduct constructor.
+     * @param ProductRepositoryInterface $productRepository
+     * @param ProductInterfaceFactory $productFactory
+     * @param CategoryLinkManagementInterface $categoryLinkManagement
+     * @param State $state
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     */
     public function __construct(
-        ModuleDataSetupInterface $moduleDataSetup,
         ProductRepositoryInterface $productRepository,
         ProductInterfaceFactory $productFactory,
         CategoryLinkManagementInterface $categoryLinkManagement,
-        CategoryRepositoryInterface $categoryRepository,
-        State $state
+        State $state,
+        SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemsSaveInterface $sourceItemsSaveInterface
     ) {
-        $this->moduleDataSetup = $moduleDataSetup;
         $this->productRepository = $productRepository;
         $this->productFactory = $productFactory;
         $this->categoryLinkManagement = $categoryLinkManagement;
-        $this->categoryRepository = $categoryRepository;
         $this->state = $state;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
     }
 
-    public static function getDependencies()
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies(): array
     {
         return [];
     }
 
-    public function getAliases()
+    /**
+     * @inheritDoc
+     */
+    public function getAliases(): array
     {
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function apply()
     {
-        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_ADMINHTML);
+        $this->state->emulateAreaCode(
+            \Magento\Framework\App\Area::AREA_ADMINHTML,
+            function () {
+                $productData = [
+                    'sku' => 'MTS001',
+                    'name' => 'Men\'s T-Shirt',
+                    'price' => 20.00,
+                    'attribute_set_id' => 4,
+                    'status' => ProductInterface::STATUS_ENABLED,
+                    'visibility' => ProductInterface::VISIBILITY_BOTH,
+                    'type_id' => ProductType::TYPE_SIMPLE,
+                ];
 
-        $this->moduleDataSetup->startSetup();
+                $product = $this->productFactory->create(['data' => $productData]);
+                $product = $this->productRepository->save($product);
 
-        $productData = [
-            'sku' => 'MTS001',
-            'name' => 'Men\'s T-Shirt',
-            'price' => 20.00,
-            'attribute_set_id' => 4,
-            'status' => ProductInterface::STATUS_ENABLED,
-            'visibility' => ProductInterface::VISIBILITY_BOTH,
-            'type_id' => \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE,
-        ];
+                $categoryId = 4;
+                $this->categoryLinkManagement->assignProductToCategories($product->getSku(), [$categoryId]);
 
-        $product = $this->productFactory->create(['data' => $productData]);
-        $product = $this->productRepository->save($product);
-
-        $categoryId = 4;
-        $this->categoryLinkManagement->assignProductToCategories($product->getSku(), [$categoryId]);
-
-        $this->moduleDataSetup->endSetup();
+                // Set inventory for the product
+                $this->setProductInventory($product->getSku(), 100); // Setting quantity as 100
+            }
+        );
     }
 
-    public function revert()
+    /**
+     * Set inventory for the product
+     *
+     * @param string $sku
+     * @param int $quantity
+     * @throws LocalizedException
+     */
+    protected function setProductInventory(string $sku, int $quantity)
     {
-        $this->moduleDataSetup->startSetup();
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setQuantity($quantity);
+        $sourceItem->setSku($sku);
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
 
-        $this->moduleDataSetup->endSetup();
+        $this->sourceItemsSaveInterface->execute([$sourceItem]);
     }
 }
